606108e7e14d72e44825bb3cd1fee3b4d9c64399
MaximeHil <hilberer.maxime@gmail.com>
2016-12-13 10:10:45 +0100

    Affichage de la connexion
    Permet à l'utilisateur de se connecter au site.

---
+++ Site/connexion.php
@@ -0,0 +1,19 @@
+<html>
+	<head>	
+	</head>
+
+	<link rel="stylesheet" href="connexion.css" />
+	
+	<body>
+	
+	<form action="rechercheService.php" method="get">
+	<div id="div1">
+		<p>Login : <input type="text" name="login" /></p>
+		<div id="mdp"><p>Mot de passe : <input type="text" name="mdp" /></p></div>
+		<p><input type="submit" value="Connecter"></p>
+	</div>
+	<div id="connect"></div>
+	</form>
+	</body>
+	
+</html>
