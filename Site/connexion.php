6249166e681348c270fb561b82cfb08b14b1e46d
blue59610 <kevin.porez@live.fr>
2017-01-03 10:46:13 +0100

    Modification connexion
    et ordonnances west

--- Site/connexion.php
+++ Site/connexion.php
@@ -1,41 +1,12 @@
-<<<<<<< HEAD
-<html>
-	<head>
-	</head>
-
-	<link rel="stylesheet" href="connexion.css" />
-
-	<body>
-
-	<form action="rechercheService.php" method="get">
-	<div id="div1">
-		Login : <input type="text" name="login" />  Mot de passe : <input type="text" name="mdp" />&nbsp;&nbsp;
-		<input type="submit" value="Connecter"></p>
-	</div>
-	<div id="connect"></div>
-	</form>
-	</body>
-
-</html>
-=======
-
-	<form action="indextest2.php" method="post">
+<div id="connect">
+	<form action="index.php" method="post">
 		<div id="div1">
-			<p>
-				Login : 
-				<input type="text" name="Pseudo" />
-			</p>
-			<div id="mdp">
-				<p>
-					Mot de passe : 
-					<input type="text" name="Password" />
-				</p>
-			</div>
-			<p>
-				<input type="submit" value="Valider" name="btn_valider">
-			</p>
-		</div>
-		<div id="connect">
+			Login : 
+			<input type="text" name="Pseudo" />  
+			Mot de passe : 
+			<input type="text" name="Password" />
+			<input type="submit" value="Connecter" name="connexion"></p>
 		</div>
 	</form>
->>>>>>> origin/master
+</div>
+	
